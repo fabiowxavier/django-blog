713087458d4bf1efaf71c32f51d38db87074ed17
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-18 12:04:49 +0000

    fix bugs

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
+# Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -10,51 +12,30 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    content = models.TextField(default="No content")
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-
     class Meta:
         ordering = ["-created_on"]
+
     def __str__(self):
-        return f"{self.title} | written by {self.author}"  
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+    related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now_add=True)    
+
     class Meta:
         ordering = ["created_on"]
-    def __str__(self):
-        return f"{self.body} | written by {self.author}"  
-
-class Event(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField()
-    location = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
-
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
-    )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
-
-    def __str__(self):
-        return f"Ticket for {self.ticket_holder} to {self.event.name}"
+        return f"Comment {self.body} by {self.author}"
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Event
 from .models import Post
 
-
 # Create your views here.
-class EventsList(generic.ListView):
-
-
-    model = Event
-    template_name = "index.html"
-    paginate_by = 12
-
-
-
-
-def event_detail(request, event_id):
-    
-    queryset = Event.objects.all()
-    event = get_object_or_404(queryset, id=event_id)
-
-    return render(
-        request,
-        "events/event_detail.html",
-        {"event": event}
-    )
 
 
 class PostList(generic.ListView):
@@ -46,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post, "coder": "Matt Rudge"},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
