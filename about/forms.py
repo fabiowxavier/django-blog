ab014f7b6a991989088148951a9c2b3df0615c86
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-19 11:47:06 +0000

    fix bug on form.py on about and blog

--- about/forms.py
+++ about/forms.py
@@ -1,14 +1,8 @@
-from django import forms
 from .models import CollaborateRequest
+from django import forms
 
 
 class CollaborateForm(forms.ModelForm):
-    """
-    Form class for users to request a collaboration 
-    """
     class Meta:
-        """
-        Specify the django model and order of the fields
-        """
         model = CollaborateRequest
         fields = ('name', 'email', 'message')
